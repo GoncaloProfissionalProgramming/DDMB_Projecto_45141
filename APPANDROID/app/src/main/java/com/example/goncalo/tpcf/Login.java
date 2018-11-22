dad85ea00e77554dfec7e521a42e55a5657ea2d8
Goncalo Cruz <goncalocruzprofissional@gmail.com>
2018-11-22 00:37:14 +0000

    Layout

--- APPANDROID/app/src/main/java/com/example/goncalo/tpcf/Login.java
+++ APPANDROID/app/src/main/java/com/example/goncalo/tpcf/Login.java
@@ -1,13 +1,91 @@
 package com.example.goncalo.tpcf;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Login extends AppCompatActivity {
+
+    private Button login;
+    private Button registar;
+    private Button ForgotPass;
+    private EditText txtemail;
+    private EditText txtpassword;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+        login=findViewById(R.id.buttonLogin);
+        registar=findViewById(R.id.buttonRegistar);
+        ForgotPass=findViewById(R.id.buttonForgotPass);
+        txtemail= findViewById(R.id.email);
+        txtpassword=findViewById(R.id.password);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email= txtemail.getText().toString();
+                String password= txtpassword.getText().toString();
+
+
+                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Toast.makeText(Login.this, "Login com sucesso", Toast.LENGTH_SHORT).show();
+                                    FirebaseUser user = mAuth.getCurrentUser();
+
+                                } else {
+                                    // If sign in fails, display a message to the user.
+
+                                    Toast.makeText(Login.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                }
+
+                                // ...
+                            }
+                        });
+            }
+        });
+        registar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, Registar.class);
+                startActivity(intent);
+            }
+        });
+
+        ForgotPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, ForgotPass.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
--- APPANDROID/app/src/main/java/com/example/goncalo/tpcf/Registar.java
+++ APPANDROID/app/src/main/java/com/example/goncalo/tpcf/Registar.java
@@ -1,13 +1,56 @@
 package com.example.goncalo.tpcf;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Registar extends AppCompatActivity {
-
+private Button Registar;
+private EditText txtemail;
+private EditText txtpass;
+private FirebaseAuth mAuth;
+String email,password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registar);
+        mAuth = FirebaseAuth.getInstance();
+        Registar=findViewById(R.id.buttonRegistar);
+        txtemail=findViewById(R.id.email);
+        txtpass=findViewById(R.id.password);
+
+        Registar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String password= txtpass.getText().toString();
+                String email=txtemail.getText().toString();
+                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener( new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Toast.makeText(Registar.this, "Registado com sucesso",Toast.LENGTH_LONG).show();
+
+
+                                } else {
+                                    Toast.makeText(Registar.this, "Não foi possivel registar",Toast.LENGTH_LONG).show();
+                                };
+
+                                // ...
+                            }
+                        });
+            }
+        });
     }
 }
