1ffe711f0868bd714148ba5392321ab0dd7592eb
Goncalo Cruz <goncalocruzprofissional@gmail.com>
2019-01-10 16:47:22 +0000

    webview working

--- APPANDROID/app/src/main/java/com/example/goncalo/tpcf/MainActivity.java
+++ APPANDROID/app/src/main/java/com/example/goncalo/tpcf/MainActivity.java
@@ -4,24 +4,31 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-private Button entrar;
+    private WebView webview;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        entrar=findViewById(R.id.btnEntrar);
+        webview = findViewById(R.id.webview);
 
-        entrar.setOnClickListener(new View.OnClickListener() {
+        webview.loadUrl("http://goncalocruz.pt/projetoFinal/home.php");
+
+        webview.setWebViewClient(new WebViewClient() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Login.class);
-                startActivity(intent);
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return false;
             }
         });
+
+
     }
+
 }
