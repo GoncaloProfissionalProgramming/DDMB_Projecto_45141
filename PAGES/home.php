2e4dd3d51499ea7f8819be7138ba2193e0e32bf8
Goncalo Cruz <goncalocruzprofissional@gmail.com>
2019-01-21 21:12:51 +0000

    home.php updated to pc

--- PAGES/home.php
+++ PAGES/home.php
@@ -12,9 +12,41 @@
     text-decoration: none;
     color: #0095D9;
     background-color: white;
-}
-
+    }
+    h2{
+        color:white; 
+        left:45.5%; 
+        position:absolute;
+        top:30%;
+    }
     .button{
+        position: absolute; 
+        background-color:white; 
+        left:36%; 
+        top:70%; 
+        height:5%;
+        width:25%;
+        text-align: center; 
+        border-radius:50%; 
+        border: none;
+        font-size: 16px;
+        font-weight: bold;
+        text-align: center;
+        vertical-align: center;
+        margin: 4px 2px;
+    }
+    img{
+        left:38.5%; 
+        position: absolute; 
+        top:40%; 
+        background-color: white;
+        width:20%; 
+        height:20%;
+    }
+
+    @media screen and (max-width: 600px) {
+    
+        .button{
         position: absolute; 
         background-color:white; 
         left:25%; 
@@ -29,11 +61,29 @@
         text-align: center;
         vertical-align: center;
         margin: 4px 2px;
+        }
+        
+        img{
+            left:25%; 
+            position: absolute; 
+            top:40%; 
+            background-color: white;
+            width:50%; 
+            height:20%;
+        }
+
+        h2{
+        color:white; 
+        left:36%; 
+        position:absolute;
+        top:30%;
+        }
     }
+    
     </style>
     <body>
-    <h2 style="color:white; left:35%; position: absolute; top:30%;">TPCF APP</h2>
-    <img src="../img/tpcflogo.png" alt="tpcf" width="50%" height="20%" style=" left:25%; position: absolute; top:40%; background-color: white;">
+    <h2 style="">TPCF APP</h2>
+    <img src="../img/tpcflogo.png" alt="tpcf" class="img">
     <button class="button"><a href="login.php">LOGIN</a></button>
     </body>
 </html>
